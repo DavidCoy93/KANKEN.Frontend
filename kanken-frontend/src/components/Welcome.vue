4ea7f2d85f525da9a2f17c0b7235722e29bacc74
Alvarado Munoz <X785574@NMCorp.Nissan.Biz>
2020-01-29 15:17:25 -0600

    Add component welcome

---
+++ kanken-frontend/src/components/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+    <h2 class="Kankenh2">{{ message }}</h2>
+</template>
+
+<script>
+export default {
+    name: 'Welcome',
+    props: {
+        message: String
+    }
+}
+</script>
+
+<style scoped>
+    .Kankenh2 {
+        font-family: fantasy;
+        color:seagreen;
+        background-color: black;
+    }
+</style>
